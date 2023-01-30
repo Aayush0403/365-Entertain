9ddd1141d7d710ce04a0ebd7c6bee167ac661c56
Aayush Gupta <aayushgupta0403@gmail.com>
2023-01-31 03:17:55 +0530

    Add files via upload

---
+++ 365-Entertain/js.js
@@ -0,0 +1,166 @@
+    
+    var x= document.getElementById("login");  
+    var y= document.getElementById("register");  
+    var z= document.getElementById("btn");  
+
+    function register(){
+      x.style.left="-400px";
+      y.style.left="50px";
+      z.style.left="110px";
+    }
+
+    function login(){
+      x.style.left="50px";
+      y.style.left="450px";
+      z.style.left="0";
+    }
+    var obj={
+
+         one:{
+            link:`https://en.wikipedia.org/wiki/The_Walt_Disney_Company`,
+            decsription:`The Walt Disney Company, commonly known as Disney (/ˈdɪzni/),[4] is an American multinational, mass media and entertainment conglomerate that is headquartered at the Walt Disney Studios complex in Burbank, California. Disney was founded on October 16, 1923, by brothers Walt and Roy O. Disney as Disney Brothers Studio; it also operated under the names Walt Disney Studio and Walt Disney Productions before changing its name to The Walt Disney Company in 1986. Early in its existence, the company established itself as a leader in the animation industry, with the creation of the widely popular character Mickey Mouse, who first appeared in Steamboat Willie, which used synchronized sound, to become the first post-produced sound cartoon.[5] The character would go on to become the company's mascot.
+            After becoming a major success by the early 1940s, the company diversified into live-action films, television, and theme parks in the 1950s. Following Walt Disney's death in 1966, the company's profits, especially in the animation division, began to decline. Once Disney's shareholders voted Michael Eisner as the head of the company in 1984, it became overwhelmingly successful during a period called the Disney Renaissance. In 2005, under new CEO Bob Iger, the company started to expand and acquire other corporations. Bob Chapek became the head of Disney in 2020 after Iger's retirement. Chapek was ousted in 2022 and Iger was reinstated as CEO.Disney is one of the biggest and best-known companies in the world, and has been ranked number 53 on the 2022 Fortune 500 list of biggest companies in the United States by revenue. Since its founding, the company has won 135 Academy Awards, 26 of which have been awarded to Walt. The company has been said to have produced some of the greatest films of all time, as well as revolutionizing the theme park industry. Disney has been criticized for supposed plagiarism, depicting racial stereotypes in the past, and both including and lacking LGBT-related elements in its films. The company, which has been public since 1940, trades on the New York Stock Exchange (NYSE) with ticker symbol DIS and has been a component of the Dow Jones Industrial Average since 1991. In August 2020, just under two-thirds of the stock was owned by large financial institutions.`
+
+         },
+         two:{
+            link:` https://en.wikipedia.org/wiki/Pixar`,
+            decsription:`Pixar Animation Studios (commonly known as Pixar (/ˈpɪksɑːr/) and stylized as P I X A R) is an American computer animation studio known for its critically and commercially successful computer animated feature films. It is based in Emeryville, California, United States. Since 2006, Pixar has been a subsidiary of Walt Disney Studios, which is another studio owned by The Walt Disney Company.
+            Pixar started in 1979 as part of the Lucasfilm computer division, known as the Graphics Group, before its spin-off as a corporation in 1986, with funding from Apple co-founder Steve Jobs, who became its majority shareholder.[2] Disney purchased Pixar in January 2006 at a valuation of $7.4+ billion by converting each share of Pixar stock to 2.3 shares of Disney stock.[4][5] Pixar is best known for its feature films, technologically powered by RenderMan, the company's own implementation of the industry-standard RenderMan Interface Specification image-rendering API. The studio's mascot is Luxo Jr., a desk lamp from the studio's 1986 short film of the same name. 
+            Pixar has produced 26 feature films, starting with Toy Story (1995), which is the first fully computer-animated feature film; its most recent film was Lightyear (2022). The studio has also produced many short films. As of July 2019, its feature films have earned approximately $14 billion at the worldwide box office,[6] with an average worldwide gross of $680 million per film.[7] Toy Story 3 (2010), Finding Dory (2016), Incredibles 2 (2018), and Toy Story 4 (2019) are all among the 50 highest-grossing films of all time, with Incredibles 2 being the fourth-highest-grossing animated film of all time, with a gross of $1.2 billion; the other three also grossed over $1 billion. Moreover, 15 of Pixar's films are in the 50 highest-grossing animated films of all time.`
+           
+         },
+         three:{
+            link:`https://en.wikipedia.org/wiki/Marvel_Cinematic_Universe#:~:text=The%20Marvel%20Cinematic%20Universe%20(MCU,books%20published%20by%20Marvel%20Comics.`,
+            decsription:`The Marvel Cinematic Universe (MCU) is an American media franchise and shared universe centered on a series of superhero films produced by Marvel Studios. The films are based on characters that appear in American comic books published by Marvel Comics. The franchise also includes television series, short films, digital series, and literature. The shared universe, much like the original Marvel Universe in comic books, was established by crossing over common plot elements, settings, cast, and characters.
+              Marvel Studios releases its films in groups called "Phases", with the first three phases collectively known as "The Infinity Saga" and the following three phases as "The Multiverse Saga". The first MCU film, Iron Man (2008), began Phase One, which culminated in the 2012 crossover film The Avengers. Phase Two began with Iron Man 3 (2013) and concluded with Ant-Man (2015). Phase Three began with Captain America: Civil War (2016) and concluded with Spider-Man: Far From Home (2019). Phase Four began with Black Widow (2021) and concluded with Black Panther: Wakanda Forever (2022). Ant-Man and the Wasp: Quantumania (2023) will begin Phase Five, which will end with Blade (2024), and Phase Six will begin with Deadpool 3 (2024). Phase Six and "The Multiverse Saga" will conclude with Avengers: The Kang Dynasty (2025) and Avengers: Secret Wars (2026).
+              Marvel Television expanded the universe to network television with Agents of S.H.I.E.L.D. on ABC in 2013 before further expanding to streaming television on Netflix and Hulu and to cable television on Freeform. They also produced the digital series Agents of S.H.I.E.L.D.: Slingshot. Marvel Studios began producing their own television series for streaming on Disney+, starting with WandaVision in 2021 as the beginning of Phase Four. They also expanded to television specials in Phase Four, known as Marvel Studios Special Presentations, the first of which was Werewolf by Night (2022).`
+
+         },
+         four:{
+            link:`https://en.wikipedia.org/wiki/Nat_Geo_Wild`,
+            decsription:`National Geographic Wild (shortened as Nat Geo Wild and abbreviated NGW) is a global pay television network owned by National Geographic Partners, a joint venture between The Walt Disney Company (73%) and the National Geographic Society (27%).[1] The channel primarily focuses on wildlife and natural history non-fiction programming. It is a sister network to National Geographic TV.
+            The channel first launched in Hong Kong on 1 January 2006.[2] It later launched in the United Kingdom, Turkey, Ireland, Romania, India, Vietnam, and Poland replacing the now defunct Adventure One. The channel remains the world's first bilingual wildlife service, available in English and Cantonese in the Hong Kong market as well as Tagalog in The Philippines. It launched in Latin America on 1 November 2009 as a high definition channel. In 2010, it launched in the United States.   
+            As of February 2015, approximately 57,891,000 American households (49.7% of households with television) receive Nat Geo Wild.[3]`
+
+         },
+         five:{
+            link:`https://en.wikipedia.org/wiki/Star_Wars`,
+            decsription:`Star Wars is an American epic space opera[1] multimedia franchise created by George Lucas, which began with the eponymous 1977 film[b] and quickly became a worldwide pop culture phenomenon. The franchise has been expanded into various films and other media, including television series, video games, novels, comic books, theme park attractions, and themed areas, comprising an all-encompassing fictional universe.[c] Star Wars is one of the highest-grossing media franchises of all time.
+            The original film (Star Wars), retroactively subtitled Episode IV: A New Hope (1977), was followed by the sequels Episode V: The Empire Strikes Back (1980) and Episode VI: Return of the Jedi (1983), forming the original Star Wars trilogy. Lucas later returned to the series to direct a prequel trilogy, consisting of Episode I: The Phantom Menace (1999), Episode II: Attack of the Clones (2002), and Episode III: Revenge of the Sith (2005). In 2012, Lucas sold his production company to Disney, relinquishing his ownership of the franchise. This led to a sequel trilogy, consisting of Episode VII: The Force Awakens (2015), Episode VIII: The Last Jedi (2017), and Episode IX: The Rise of Skywalker (2019).
+            All nine films of the "Skywalker Saga" were nominated for Academy Awards, with wins going to the first two releases. Together with the theatrical live action "anthology" films Rogue One (2016) and Solo (2018), the combined box office revenue of the films equated to over US$10 billion, which makes it the second-highest-grossing film franchise of all time.[3][4] Additional upcoming films are in the works, including an untitled movie from Taika Waititi and Rogue Squadron directed by Patty Jenkins, both currently without release dates.[5]`
+
+         },
+
+
+    }
+    
+    function funk(val)
+    {
+      var vidz;
+      if(val=='one')
+      {
+            vidz="videos/disney.mp4";
+            localStorage.setItem('pass',vidz);
+            localStorage.setItem('des',obj.one.decsription)
+            localStorage.setItem('link',obj.one.link)
+      }
+      if(val=='two')
+      {
+            vidz="videos/pixar.mp4";
+            localStorage.setItem('pass',vidz);
+            localStorage.setItem('des',obj.two.decsription)
+            localStorage.setItem('link',obj.two.link)
+      }
+      else if(val=='three')
+      {
+            vidz="videos/marvel.mp4";
+            localStorage.setItem('pass',vidz);
+            localStorage.setItem('des',obj.three.decsription)
+            localStorage.setItem('link',obj.three.link)
+      }
+      else if(val=='four')
+      {
+            vidz="videos/geographic.mp4";
+            localStorage.setItem('pass',vidz);
+            localStorage.setItem('des',obj.four.decsription)
+            localStorage.setItem('link',obj.four.link)
+      }
+      else if(val=='five')
+      {
+            vidz="videos/star-war.mp4"; 
+            localStorage.setItem('pass',vidz);
+            localStorage.setItem('des',obj.five.decsription)
+            localStorage.setItem('link',obj.five.link)
+      }
+      window.location.href='play.php';
+      
+   
+    }
+
+    function funcc()
+    {
+      var sorce=document.querySelector('#video');
+      var vid=document.querySelector('#vidz');
+      var des=document.querySelector('.description');
+      var link=document.querySelector('.link');
+      link.href=localStorage.getItem('link');
+      des.innerText=localStorage.getItem('des');
+      sorce.src=localStorage.getItem('pass');
+      vid.load();  
+    }
+
+    function funy(value)
+    {
+      const no= document.querySelector("#no");
+      const nt= document.querySelector("#nt");
+      const nth= document.querySelector("#nth");
+      const child =document.createElement('span')
+      child.innerHTML=`that all about it,nothing more.`
+      if(value.innerText=="Read more")
+      {
+            value.innerText='Read Less';
+            if(value.id=="anc1")
+                no.appendChild(child)
+            else if(value.id=="anc2")
+                nt.appendChild(child)
+            else if(value.id=="anc3")
+                nth.appendChild(child)
+
+      }
+       else if(value.innerText=="Read Less")
+      {
+            value.innerText='Read more';
+            if(value.id=="anc1")
+                no.innerHTML=''
+            else if(value.id=="anc2")
+                nt.innerHTML=''
+            else if(value.id=="anc3")
+                nth.innerHTML=''
+      }
+    }
+
+    function funshow(){
+      var q=document.querySelector('#shows');
+      q.innerHTML= localStorage.getItem('passvalue');
+      document.querySelector('#shows').style.textTransform="capitalize";
+    }
+    function funj()
+    {
+      var p=document.querySelector('#passvalues');
+      z=p.value;
+      localStorage.setItem('passvalue',z);
+    } 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 365-Entertain/process.php
@@ -0,0 +1,48 @@
+<?php
+$hostname="localhost";
+$username ="root";
+$password="";
+$dbname="tt";
+
+$dbc=mysqli_connect($hostname,$username,$password,$dbname) or die("couldnot connect to dtabase".mysqli_connect_error());
+mysqli_set_charset($dbc,"utf8");
+
+if($_SERVER['REQUEST_METHOD']=='POST')
+{
+   $name=$_POST['username'];
+   $mail=$_POST['email-id'];
+   $pswd=$_POST['password'];
+    if((!empty($name)and !empty($mail) and !empty($pswd)))
+    {
+       
+       
+        $result= mysqli_query($dbc,"SELECT * FROM ft WHERE NAME='$name' AND EMAIL ='$mail'");
+        $count=mysqli_num_rows($result);
+        if($count>0)
+        {
+          $row=mysqli_fetch_assoc($result);
+          if($mail==isset($row['EMAIL']) && $name==isset($row['NAME']))
+          {
+            echo "<script>alert('User already exists,Please signin.')</script>";
+          }
+        }
+        else
+        {
+         $hashed_password = password_hash($pswd, PASSWORD_DEFAULT);
+         mysqli_query($dbc,"INSERT INTO ft(NAME,EMAIL,PASSWORD) VALUES('$name','$mail','$hashed_password')");
+        }
+
+        include('index.php');
+
+    }
+    else{
+        echo "ERROR"."please fill all the values";
+    }
+
+}
+else
+{
+    echo "no form submitted";
+}
+
+?>
